d622ac6c90cbd3537695e794e4a52cb3fd6955e8
aydellast <ayudellaastuti23@gmail.com>
2026-05-16 19:45:51 +0700

    api focusmode

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 
 import authRoutes from "./routes/authRoutes";
 import taskRoutes from "./routes/taskRoutes";
+import focusRoutes from "./routes/focusRoutes";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
+app.use("/api/focus", focusRoutes);
 
 // Test Route
 app.get("/", (req, res) => {
